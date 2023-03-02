276a6a0927c7e01768761080a02ad19f7db76bc6
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-02 22:28:10 +0000

    Create resumeData.js

---
+++ src/resumeData.js
@@ -0,0 +1,74 @@
+// Define resume data as an object literal
+const resumeData = {
+  imagebaseurl: "https://github.com/amarfiguig/",
+  name: "amarfiguig",
+  role: "Developer",
+  roleDescription:
+    "I like coding .",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/amarfiguig",
+      className: "fa fa-github",
+    },
+    
+// example { name: "facebook"  url: "https://www.facebook.com/", className: "fa fa-facebook",},
+
+  ],
+  aboutme:
+    "I am student at University of Birmingham .",
+  address: "UK",
+  website: "www.code.com",
+  education: [
+    {
+      UniversityName: "Coding University",
+      specialization: "X SP",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2022",
+    },
+    {
+      UniversityName: "University",
+      specialization: "X SP",
+      MonthOfPassing: "Oct",
+      YearOfPassing: "2012",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "X Company",
+      specialization: "Work",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2020",
+    },
+
+  ],
+  skillsDescription: "skills",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "PHP",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "project",
+      imgurl: "images x.jpg",
+    },
+  ],
+};
+
+// Export the resumeData object for use in
