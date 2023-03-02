a601fc9c8158020fbfbede0829d2a7fc6254f0e7
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-02 22:39:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-// Import necessary components
-import React from 'react';
-import Header from './components/Header'; // Header component
-import About from './components/About'; // About component
-import Portfolio from './components/Portfolio'; // Portfolio component
-import ContactUs from './components/ContactUs'; // ContactUs component
-import Footer from './components/Footer'; // Footer component
+import React, { Component } from 'react';
 
-// Define the main App component
-const App = () => {
-  // Render all the necessary components and pass the data as props
-  return (
-    <>
-      <Header />
-      <About />
-      <Portfolio />
-      <ContactUs />
-      <Footer />
-    </>
-  );
+// Import all necessary components from their respective files
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+
+// Import resume data from a separate file
+import resumeData from './resumeData';
+
+// Define App component as a class that extends from Component
+class App extends Component {
+  render() {
+    // Render all components, passing resumeData as a prop to each one
+    return (
+      <div className="App">
+        <Header resumeData={resumeData} />
+        <About resumeData={resumeData} />
+        <Resume resumeData={resumeData} />
+        <Portfolio resumeData={resumeData} />
+        <ContactUs resumeData={resumeData} />
+        <Footer resumeData={resumeData} />
+      </div>
+    );
+  }
 }
 
-// Export the App component as the default export
+// Export App component as the default export
 export default App;
