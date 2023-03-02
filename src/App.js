c912f596b8d2b180c92067681e70fe74309f9c57
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-02 13:42:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-// Import the CSS file for styling
-import './index.css';
+// Import necessary components and data
+import React from 'react';
+import Header from './components/Header'; // Header component
+import About from './components/About'; // About component
+import Resume from './components/Resume'; // Resume component
+import Portfolio from './components/Portfolio'; // Portfolio component
+import ContactUs from './components/ContactUs'; // ContactUs component
+import Footer from './components/Footer'; // Footer component
+import resumeData from './resumeData'; // Data for the resume
 
-// Import the PortfolioContainer component
-import PortfolioContainer from "./components/PortfolioContainer";
-
-// Define the App component and render the PortfolioContainer component
-const App = () => <PortfolioContainer />;
+// Define the main App component
+const App = () => {
+  // Render all the necessary components and pass the resume data as props
+  return (
+    <>
+      <Header resumeData={resumeData} /> // Header component with resume data as prop
+      <About resumeData={resumeData} /> // About component with resume data as prop
+      <Resume resumeData={resumeData} /> // Resume component with resume data as prop
+      <Portfolio resumeData={resumeData} /> // Portfolio component with resume data as prop
+      <ContactUs resumeData={resumeData} /> // ContactUs component with resume data as prop
+      <Footer resumeData={resumeData} /> // Footer component with resume data as prop
+    </>
+  );
+}
 
 // Export the App component as the default export
 export default App;
