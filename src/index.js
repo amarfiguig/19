049f1506f69012d2e08f12b0b9ef1cae0adee8eb
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-02 14:31:50 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-// Import necessary components and data
+// Import necessary components
 import React from 'react';
 import Header from './components/Header'; // Header component
 import About from './components/About'; // About component
-import Resume from './components/Resume'; // Resume component
 import Portfolio from './components/Portfolio'; // Portfolio component
 import ContactUs from './components/ContactUs'; // ContactUs component
 import Footer from './components/Footer'; // Footer component
-import resumeData from './resumeData'; // Data for the resume
 
 // Define the main App component
 const App = () => {
-  // Render all the necessary components and pass the resume data as props
+  // Render all the necessary components and pass the data as props
   return (
     <>
-      <Header resumeData={resumeData} /> // Header component with resume data as prop
-      <About resumeData={resumeData} /> // About component with resume data as prop
-      <Resume resumeData={resumeData} /> // Resume component with resume data as prop
-      <Portfolio resumeData={resumeData} /> // Portfolio component with resume data as prop
-      <ContactUs resumeData={resumeData} /> // ContactUs component with resume data as prop
-      <Footer resumeData={resumeData} /> // Footer component with resume data as prop
+      <Header />
+      <About />
+      <Portfolio />
+      <ContactUs />
+      <Footer />
     </>
   );
 }
